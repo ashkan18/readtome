7e3cd94b0b50d5c2af44800a4fd6264946ac9eda
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2020-11-26 21:52:22 -0500

    fix css

--- assets/js/components/map_component.tsx
+++ assets/js/components/map_component.tsx
@@ -1,118 +1,117 @@
-import * as React from "react"
-import ReactMapboxGl, { Feature, Layer, Marker, Popup } from 'react-mapbox-gl';
+import * as React from "react";
+import ReactMapboxGl, { Feature, Layer, Marker, Popup } from "react-mapbox-gl";
 
-import Coordinate from "../models/coordinate"
-import BookInstance from "../models/book_instance"
-import {GeolocateControl} from "mapbox-gl"
+import Coordinate from "../models/coordinate";
+import BookInstance from "../models/book_instance";
+import { GeolocateControl } from "mapbox-gl";
 import BookInstanceDetail from "./book_instance_detail";
 import { svg } from "./icon";
 
-
 const Map = ReactMapboxGl({
-  accessToken: "pk.eyJ1IjoiYXNoa2FuMTgiLCJhIjoiY2pzdnk5eGRpMGMxcTN5bzRsOHRjdDR2cCJ9.qaLMKiKsDDLnMPLJ-s4rIQ",
+  accessToken:
+    "pk.eyJ1IjoiYXNoa2FuMTgiLCJhIjoiY2pzdnk5eGRpMGMxcTN5bzRsOHRjdDR2cCJ9.qaLMKiKsDDLnMPLJ-s4rIQ",
   minZoom: 8,
   maxZoom: 15,
 });
 
 const mapStyle = {
-  flex: 1
+  flex: 1,
 };
 
 const flyToOptions = {
-  speed: 0.8
+  speed: 0.8,
 };
 
-const layoutLayer = { 'icon-image': 'londonCycle' };
+const layoutLayer = { "icon-image": "londonCycle" };
 
 // Create an image for the Layer
 const image = new Image();
-image.src = 'data:image/svg+xml;charset=utf-8;base64,' + btoa(svg);
-const images: any = ['londonCycle', image];
+image.src = "data:image/svg+xml;charset=utf-8;base64," + btoa(svg);
+const images: any = ["londonCycle", image];
 
-
-interface Props{
-  bookInstances: Array<BookInstance>
-  initialCoordinate: Coordinate
-  onStyleLoad?: (map: any) => any
+interface Props {
+  bookInstances: Array<BookInstance>;
+  initialCoordinate: Coordinate;
+  onStyleLoad?: (map: any) => any;
 }
 
 interface State {
-  bookInstance?: BookInstance
-  zoom: number
-  centerLat: number
-  centerLng: number
+  bookInstance?: BookInstance;
+  zoom: number;
+  centerLat: number;
+  centerLng: number;
 }
 
 interface Action {
-  type: string
-  item?: BookInstance
+  type: string;
+  item?: BookInstance;
 }
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "INSTANCE_SELECTED":
-      const bookInstance = action.item
-      return {...state, 
+      const bookInstance = action.item;
+      return {
+        ...state,
         bookInstance: bookInstance,
         zoom: 15,
         centerLat: bookInstance.location.lat,
-        centerLng: bookInstance.location.lng 
-      }
+        centerLng: bookInstance.location.lng,
+      };
     case "RESET_SELECT":
       if (state.selectedBookInstance) {
-        return {...state, bookInstance: undefined}
+        return { ...state, bookInstance: undefined };
       } else {
-        return state
+        return state;
       }
     default:
-      return state
-    }
-}
+      return state;
+  }
+};
 
 export const MapComponent = (props: Props) => {
-  
   const geoLocation = new GeolocateControl({
     positionOptions: {
-      enableHighAccuracy: false
+      enableHighAccuracy: false,
     },
-    trackUserLocation: false
-  })
+    trackUserLocation: false,
+  });
 
   const initialState = {
     bookInstance: undefined,
     zoom: 13,
     centerLat: props.initialCoordinate.lat,
-    centerLng: props.initialCoordinate.lng
-  }
+    centerLng: props.initialCoordinate.lng,
+  };
 
-  
+  const [state, dispatch] = React.useReducer<React.Reducer<State, Action>>(
+    reducer,
+    initialState
+  );
 
-  const [state, dispatch] = React.useReducer<React.Reducer<State, Action>> (reducer, initialState)
-  
   const onDrag = () => {
     if (state.bookInstance) {
-      dispatch({ type: 'RESET_SELECT' })
+      dispatch({ type: "RESET_SELECT" });
     }
-  }
+  };
   const onToggleHover = (cursor: string, { map }: { map: any }) => {
     map.getCanvas().style.cursor = cursor;
-  }
+  };
 
   const onMarkerClick = (bookInstance: BookInstance) => {
-    dispatch({ type: 'INSTANCE_SELECTED', item: bookInstance })
-  }
-
+    dispatch({ type: "INSTANCE_SELECTED", item: bookInstance });
+  };
 
   const onStyleLoad = (map: any) => {
     const { onStyleLoad } = props;
     map.addControl(geoLocation);
     setTimeout(() => {
-      geoLocation.trigger()
+      geoLocation.trigger();
     }, 500);
     return onStyleLoad && onStyleLoad(map);
-  }
-  
-  return(
+  };
+
+  return (
     <Map
       style="mapbox://styles/ashkan18/ckhzblkri2mr719n224l706o0"
       containerStyle={mapStyle}
@@ -121,28 +120,31 @@ export const MapComponent = (props: Props) => {
       onDrag={onDrag}
       center={[state.centerLng, state.centerLat]}
       zoom={[state.zoom]}
-      >
-        <Layer type="symbol" id="marker" layout={layoutLayer} images={images}>
-          {props.bookInstances?.map((bi, index) => (
-            <Feature
-              key={bi.id}
-              onMouseEnter={onToggleHover.bind(this, 'pointer')}
-              onMouseLeave={onToggleHover.bind(this, '')}
-              onClick={() => onMarkerClick(bi)}
-              coordinates={[bi.location.lng, bi.location.lat]}
-            />
-          ))}
-        </Layer>
-        
-      { state.bookInstance &&
-        <Popup
-          coordinates={[state.bookInstance.location.lng, state.bookInstance.location.lat]}
-          anchor="bottom"
-          offset={[0, -15]}>
-          <BookInstanceDetail bookInstance={state.bookInstance}/>
-        </Popup>
-      }
-    </Map>
-  )
+    >
+      <Layer type="symbol" id="marker" layout={layoutLayer} images={images}>
+        {props.bookInstances?.map((bi, index) => (
+          <Feature
+            key={bi.id}
+            onMouseEnter={onToggleHover.bind(this, "pointer")}
+            onMouseLeave={onToggleHover.bind(this, "")}
+            onClick={() => onMarkerClick(bi)}
+            coordinates={[bi.location.lng, bi.location.lat]}
+          />
+        ))}
+      </Layer>
 
-}
+      {state.bookInstance && (
+        <Popup
+          coordinates={[
+            state.bookInstance.location.lng,
+            state.bookInstance.location.lat,
+          ]}
+          anchor="bottom"
+          offset={[0, -15]}
+        >
+          <BookInstanceDetail bookInstance={state.bookInstance} />
+        </Popup>
+      )}
+    </Map>
+  );
+};
