424f3544fdf3d4438f2f44ca9e868f3edb340b34
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2020-12-31 18:36:20 -0500

    Fix signup

--- assets/js/app_routes.tsx
+++ assets/js/app_routes.tsx
@@ -1,10 +1,9 @@
 import * as React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
-import {Login} from "./pages/login";
-import Signup from "./pages/sign_up";
+import { Login } from "./pages/login";
+import { Signup } from "./pages/sign_up";
 import { Main } from "./pages/main";
-
 
 export default class AppRoutes extends React.Component<{}, {}> {
   public constructor(props: any, context: any) {
@@ -15,14 +14,8 @@ export default class AppRoutes extends React.Component<{}, {}> {
   public render() {
     return (
       <Switch>
-        <Route
-          path="/login"
-          render={() => <Login />}
-        />
-        <Route
-          path="/signup"
-          render={() => <Signup authenticate={this.authenticate} />}
-        />
+        <Route path="/login" render={() => <Login />} />
+        <Route path="/signup" render={() => <Signup />} />
         <Route path="/" render={() => <Main />} />
       </Switch>
     );
--- assets/js/pages/sign_up.tsx
+++ assets/js/pages/sign_up.tsx
@@ -1,26 +1,11 @@
 import * as React from "react";
 import { Redirect } from "react-router";
 import axios from "axios";
-import { Input, Button, InputOnChangeData } from "semantic-ui-react";
+import { Input, Button, InputOnChangeData, Form } from "semantic-ui-react";
 import { SyntheticEvent, FormEvent } from "react";
 import MainLayout from "../components/main_layout";
 import { Header } from "../components/header";
 import styled from "styled-components";
-
-interface Props {
-  authenticate: any;
-}
-
-interface State {
-  loading: boolean;
-  isLoggedIn: boolean;
-  name: string;
-  email: string;
-  userName: string;
-  password: string;
-  confirmPassword: string;
-  error: string;
-}
 
 const SignUpForm = styled.div`
   display: flex;
@@ -30,126 +15,144 @@ const SignUpForm = styled.div`
   padding-top: 50px;
 `;
 
-export default class Signup extends React.Component<Props, State> {
-  public constructor(props: Props, context: any) {
-    super(props, context);
+interface State {
+  loading: boolean;
+  signedUp: boolean;
+  error?: string;
+}
+interface Action {
+  type: string;
+  data?: string;
+}
 
-    this.handleUsernameChange = this.handleUsernameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleEmailChange = this.handleEmailChange.bind(this);
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {
-      loading: false,
-      isLoggedIn: false,
-      userName: "",
-      password: "",
-      confirmPassword: "",
-      error: "",
-      name: "",
-      email: "",
-    };
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "ATTEMPT_SIGNUP":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "SUCCESSFUL_SIGNUP":
+      return {
+        ...state,
+        loading: false,
+        signedUp: true,
+      };
+    case "SIGNUP_FAILED":
+      return {
+        ...state,
+        loading: false,
+        signedUp: false,
+        error: action.data,
+      };
+    default:
+      return state;
   }
+};
 
-  public handleSubmit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    this.setState({ loading: true });
+export const Signup = () => {
+  const [name, setName] = React.useState<string | null>();
+  const [email, setEmail] = React.useState<string | null>();
+  const [username, setUsername] = React.useState<string | null>();
+  const [password, setPassword] = React.useState<string | null>();
+  const initialState: State = {
+    loading: false,
+    signedUp: false,
+  };
+  const [state, dispatch] = React.useReducer<React.Reducer<State, Action>>(
+    reducer,
+    initialState
+  );
+
+  const signUp = () => {
     axios
       .post("/api/signup", {
         user: {
-          name: this.state.name,
-          email: this.state.email,
-          username: this.state.userName,
-          password: this.state.password,
+          name: name,
+          email: email,
+          username: username,
+          password: password,
         },
       })
       .then((response) => {
-        this.props.authenticate(response.data.token);
-        this.setState({ isLoggedIn: true, loading: false });
+        dispatch({ type: "SUCCESSFUL_SIGNUP" });
       })
       .catch((error) => {
-        this.setState({
-          error: "Username and Password don't match, please retry.",
-          loading: false,
+        dispatch({
+          type: "SIGNUP_FAILED",
+          data: "Username and Password don't match, please retry.",
         });
       });
-  }
+  };
 
-  private handleUsernameChange(
-    _event: SyntheticEvent<HTMLInputElement, Event>,
-    data: InputOnChangeData
-  ) {
-    this.setState({ userName: data.value });
-  }
-  private handleNameChange(
-    _event: SyntheticEvent<HTMLInputElement, Event>,
-    data: InputOnChangeData
-  ) {
-    this.setState({ name: data.value });
-  }
-  private handleEmailChange(
-    _event: SyntheticEvent<HTMLInputElement, Event>,
-    data: InputOnChangeData
-  ) {
-    this.setState({ email: data.value });
-  }
-  private handlePasswordChange(
-    _event: SyntheticEvent<HTMLInputElement, Event>,
-    data: InputOnChangeData
-  ) {
-    this.setState({ password: data.value });
-  }
-
-  public render() {
-    return (
-      <MainLayout>
-        <Header me={null} />
-        <SignUpForm>
-          <div className="error">{this.state.error}</div>
-          <form onSubmit={this.handleSubmit}>
-            <Input
-              error={Boolean(this.state.error)}
-              type="text"
-              placeholder="Name"
-              onChange={this.handleNameChange}
-            />
-            <Input
-              error={Boolean(this.state.error)}
-              type="text"
-              placeholder="Username"
-              onChange={this.handleUsernameChange}
-            />
-            <Input
-              error={Boolean(this.state.error)}
-              type="email"
-              placeholder="Email"
-              onChange={this.handleEmailChange}
-            />
-            <Input
-              error={Boolean(this.state.error)}
-              type="email"
-              placeholder="Confirm Email"
-              onChange={this.handlePasswordChange}
-            />
-            <Input
-              error={Boolean(this.state.error)}
-              type="password"
-              placeholder="Password"
-              onChange={this.handlePasswordChange}
-            />
-            <Input
-              error={Boolean(this.state.error)}
-              type="password"
-              placeholder="Confirm Password"
-              onChange={this.handlePasswordChange}
-            />
-            <Button basic color="orange" onSubmit={this.handleSubmit}>
-              Signup
-            </Button>
-          </form>
-          {this.state.isLoggedIn && <Redirect to="/" />}
-        </SignUpForm>
-      </MainLayout>
-    );
-  }
-}
+  return (
+    <MainLayout>
+      <Header me={null} />
+      <SignUpForm>
+        <Form onSubmit={() => signUp()}>
+          <Form.Group width={2}>
+            <Form.Field>
+              <Input
+                type="text"
+                placeholder="Name"
+                onChange={(event) => setName(event.target.value)}
+                disabled={state.loading}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Input
+                type="text"
+                placeholder="Username"
+                onChange={(event) => setUsername(event.target.value)}
+                disabled={state.loading}
+              />
+            </Form.Field>
+          </Form.Group>
+          <Form.Group width={2}>
+            <Form.Field>
+              <Input
+                type="email"
+                placeholder="Email"
+                onChange={(event) => setEmail(event.target.value)}
+                disabled={state.loading}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Input
+                type="email"
+                placeholder="Confirm Email"
+                onChange={(event) => setEmail(event.target.value)}
+                disabled={state.loading}
+              />
+            </Form.Field>
+          </Form.Group>
+          <Form.Group width={2}>
+            <Form.Field>
+              <Input
+                type="password"
+                placeholder="Password"
+                onChange={(event) => setPassword(event.target.value)}
+                disabled={state.loading}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Input
+                type="password"
+                placeholder="Confirm Password"
+                onChange={(event) => setPassword(event.target.value)}
+                disabled={state.loading}
+              />
+            </Form.Field>
+          </Form.Group>
+          <Form.Group width={2}>
+            <Form.Field>
+              <Button basic type="submit" color="orange">
+                Signup
+              </Button>
+            </Form.Field>
+          </Form.Group>
+        </Form>
+      </SignUpForm>
+    </MainLayout>
+  );
+};
